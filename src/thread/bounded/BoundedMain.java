c0f7ecad22ad63aa439f9509298410e265b6f24f
bagjuna <juna.park124@gmail.com>
2025-09-25 16:20:33 +0900

    mend

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -10,11 +10,23 @@ public class BoundedMain {
 	public static void main(String[] args) {
 
 		//1. BoundedQueue 선택
-		BoundedQueue queue = new BoundedQueueV1(2);
+		// BoundedQueue queue = new BoundedQueueV1(2);
+		BoundedQueue queue = new BoundedQueueV2(2);
 
 		// 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
-		// producerFirst(queue);  // 생산자 먼저 실행
-		consumerFirst(queue);  // 소비자 먼저 실행
+		producerFirst(queue);  // 생산자 먼저 실행
+		// consumerFirst(queue);  // 소비자 먼저 실행
+	}
+
+	private static void producerFirst(BoundedQueue queue) {
+
+		log("== [생산자 먼저 실행] 시작, " + queue.getClass().getSimpleName() + " ==");
+		List<Thread> threads = new ArrayList<>();
+		startProducer(queue, threads);
+		printAllState(queue, threads);
+		startConsumer(queue, threads);
+		printAllState(queue, threads);
+		log("== [생산자 먼저 실행] 종료 =="+ queue.getClass().getSimpleName() + " ==");
 	}
 
 	private static void consumerFirst(BoundedQueue queue) {
@@ -25,16 +37,6 @@ public class BoundedMain {
 		startProducer(queue, threads);
 		printAllState(queue, threads);
 		log("== [소비자 먼저 실행] 종료 =="+ queue.getClass().getSimpleName() + " ==");
-	}
-
-	private static void producerFirst(BoundedQueue queue) {
-		log("== [생산자 먼저 실행], " + queue.getClass().getSimpleName() + " ==");
-		List<Thread> threads = new ArrayList<>();
-		startProducer(queue, threads);
-		printAllState(queue, threads);
-		startConsumer(queue, threads);
-		printAllState(queue, threads);
-		log("== [생산자 먼저 실행] 종료 =="+ queue.getClass().getSimpleName() + " ==");
 	}
 
 	private static void startProducer(BoundedQueue queue, List<Thread> threads) {
@@ -52,9 +54,9 @@ public class BoundedMain {
 		System.out.println();
 		log("소비자 시작");
 		for (int i = 1; i <= 3; i++) {
-			Thread thread = new Thread(new ConsumerTask(queue), "consumer" + i);
-			threads.add(thread);
-			thread.start();
+			Thread consumer = new Thread(new ConsumerTask(queue), "consumer" + i);
+			threads.add(consumer);
+			consumer.start();
 			sleep(100);
 		}
 	}

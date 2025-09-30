9b80fb8e1f9acc54cd13d7efb3194869aed63202
bagjuna <juna.park124@gmail.com>
2025-09-30 14:05:50 +0900

    Executor - Feature1

---
+++ src/thread/executor/future/CallableMainV1.java
@@ -0,0 +1,38 @@
+package thread.executor.future;
+
+import static thread.util.MyLogger.*;
+import static thread.util.ThreadUtils.*;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableMainV1 {
+
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		ExecutorService es = Executors.newFixedThreadPool(1);
+		Future<Integer> future = es.submit(new MyCallable());
+		Integer result = future.get();
+		log("result value = " + result);
+		es.close();
+	}
+
+	static class MyCallable implements Callable<Integer> {
+
+		@Override
+		public Integer call() {
+			log("Callable 시작");
+
+			sleep(2000);
+			int value = new Random().nextInt(10);
+			log("created value = " + value);
+			log("Callable 종료");
+
+			return value;
+		}
+
+	}
+}

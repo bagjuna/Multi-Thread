0025afda1d1e68befc68c243aa656c46d10259d7
bagjuna <juna.park124@gmail.com>
2025-10-02 15:55:21 +0900

    ExecutorService - 스레드 풀 전략 3

---
+++ src/thread/executor/reject/RejectMainV3.java
@@ -0,0 +1,24 @@
+package thread.executor.reject;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import thread.executor.RunnbleTask;
+
+public class RejectMainV3 {
+	public static void main(String[] args) {
+
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS,
+			new SynchronousQueue<>(), new ThreadPoolExecutor.CallerRunsPolicy());
+
+		executor.submit(new RunnbleTask("task1"));
+		executor.submit(new RunnbleTask("task2"));
+		executor.submit(new RunnbleTask("task3"));
+		executor.submit(new RunnbleTask("task4"));
+		
+		executor.close();
+	}
+
+	
+}
